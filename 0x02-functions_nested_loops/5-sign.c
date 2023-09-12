4d96a92f522d64f74c7d1ff354fa4f98ce49458b
Shehab-Mansour <140319620+Shehab-Mansour@users.noreply.github.com>
2023-09-12 13:54:58 +0300

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,9 +13,9 @@ int print_sign(int n)
 	{
 		_putchar(43);
 		return (1);
-	} 
+	}
 	else if (n == 0)
-	{	
+	{
 		_putchar(48);
 		return (0);
 	}
@@ -23,5 +23,5 @@ int print_sign(int n)
 	{
 		_putchar(45);
 		return (-1);
-	}	
+	}
 }
