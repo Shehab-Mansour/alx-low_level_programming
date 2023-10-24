7ac184ac084234798eb0db9b573f7b095bfe4d80
Shehab-Mansour <140319620+Shehab-Mansour@users.noreply.github.com>
2023-10-24 23:55:56 +0300

    ww

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,23 +1,50 @@
 #include "lists.h"
 
 /**
- * reverse_listint - prints a listint_t linked list.
- * @head: pointer to the list.
- * Return: number of nodes in the list.
- **/
+ * print_listint_safe - Function that prints a linked list
+ * @head: Beginning of the list
+ * Return: Number of nodes
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t pichu = 0;
-	const listint_t *aux_node = head;
+	unsigned int i = 0, j = 0, k = 0, bool = 0;
+	listint_t *slow, *fast, *temp;
 
-	if (!head)
-		exit(98);
-
-	while (aux_node)
+	if (head == NULL)
+		printf("0\n"), exit(98);
+	slow = (listint_t *)head, fast = (listint_t *)head;
+	while (slow && fast && fast->next)
 	{
-		printf("[%p] %i\n", (void *)aux_node, aux_node->n);
-		aux_node = aux_node->next;
-		pichu++;
+		slow = slow->next, fast = fast->next->next;
+		if (slow == fast)
+		{
+			bool = 1;
+			break;
+		}
 	}
-	return (pichu);
+	if (bool == 1)
+	{
+		for (j = 1; fast->next != slow; j++)
+			fast = fast->next;
+		slow = (listint_t *)head, fast = (listint_t *)head;
+		for (; k < j; k++)
+			fast = fast->next;
+		while (1)
+		{
+			fast = fast->next, slow = slow->next;
+			if (fast->next == slow->next)
+			{
+				temp = slow->next, fast->next = NULL;
+				break;
+			}
+		}
+	}
+	while (head)
+	{
+		(head == NULL) ? (void)exit(98) : (void)head;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next, i++;
+	}
+	(bool == 1) ? printf("-> [%p] %d\n", (void *)temp, temp->n) : (int)bool;
+	return (i);
 }
